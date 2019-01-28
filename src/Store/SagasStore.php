3d09a6883dcb3e27d0b381c21233a06b38b9906b
desperado <desperado@minsk-info.ru>
2019-01-28 12:46:16 +0300

    change saga store contract

--- src/Store/SagasStore.php
+++ src/Store/SagasStore.php
@@ -25,16 +25,13 @@ interface SagasStore
      * Obtain exists saga
      *
      * @param SagaId $id
-     * @param bool   $ignoreExpiration If specified, the expiration date will be ignored (the saga will be in read-only mode);
-     *                                 Otherwise, when trying to load an expired saga, an exception will be thrown
      *
      * @return Promise<\ServiceBus\Sagas\Saga|null>
      *
      * @throws \ServiceBus\Sagas\Store\Exceptions\SagasStoreInteractionFailed Database interaction error
      * @throws \ServiceBus\Sagas\Store\Exceptions\SagaSerializationError Error while deserializing saga
-     * @throws \ServiceBus\Sagas\Store\Exceptions\LoadedExpiredSaga
      */
-    public function obtain(SagaId $id, bool $ignoreExpiration): Promise;
+    public function obtain(SagaId $id): Promise;
 
     /**
      * Save the new saga
